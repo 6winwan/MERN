edd444998727b529507db6e207aba4e12c0a3379
Seungwan Yuk <6winwan@gmail.com>
2018-12-28 17:38:05 -0700

    Redux and Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom' ;
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions'
+
 import { Provider } from 'react-redux';
 import store from './store'
 
@@ -11,17 +15,37 @@ import Login from './components/auth/Login';
 
 import './App.css';
 
+// Check for token
+if(localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired Token
+  const currentTime = Date.now()/1000;
+  if(decoded.exp < currentTime) {
+    // Logout User
+    store.dispatch(logoutUser());
+    // Todo: clear current profile
+    // Redirect to login
+    window.location.href = '/login';
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <Provider store={ store }>
         <Router>
           <div className="App">
-            <Route exact path="/" component={Navbar} />
+            <Navbar />
             <Route exact path="/" component={Landing} />
               <div className="container">
-                <Route exat path="/register" component={Register} />
-                <Route exat path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
               </div>
               <Footer />
           </div>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import PropTypes from 'prop-types'
+import { withRouter } from "react-router-dom";
 import classnames from 'classnames';
+import { connect } from 'react-redux'
+import { registerUser } from '../../actions/authActions'
 
 class Register extends Component {
   constructor() {
@@ -16,6 +19,18 @@ class Register extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentDidMount(){
+    if(this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard')
+    }
+  }
+  
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.errors) {
+      this.setState({errors: nextProps.errors});
+    }
+  }
+
   onChange(e) {
     this.setState({[e.target.name]: e.target.value});
   }
@@ -27,11 +42,9 @@ class Register extends Component {
       email: this.state.email,
       password: this.state.password,
       password2: this.state.password2
-    }
-  
-    axios.post('/api/users/register', newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({errors: err.response.data}));
+    };
+
+    this.props.registerUser(newUser, this.props.history);
   }
 
   render() {
@@ -92,4 +105,15 @@ class Register extends Component {
   }
 }
 
-export default Register;
+Register.propType = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors:PropTypes.object.isRequired
+};
+
+const mapSatateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapSatateToProps, { registerUser })(withRouter(Register));
