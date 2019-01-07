78ade1a3467a98ecbaf0d7571bc399b73fa89c72
Seungwan Yuk <6winwan@gmail.com>
2019-01-06 17:00:45 -0700

    Post and Comment

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -2,11 +2,17 @@ import axios from 'axios';
 
 import {
   ADD_POST,
-  GET_ERRORS
+  GET_ERRORS,
+  GET_POSTS,
+  POST_LOADING,
+  DELETE_POST,
+  GET_POST,
+  CLEAR_ERRORS
 } from './types';
 
 // ADD POST
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
   axios
     .post('/api/posts', postData)
     .then(res => 
@@ -17,8 +23,144 @@ export const addPost = postData => dispatch => {
     )
     .catch(err => 
       dispatch({
-        type:GET_ERRORS,
-        paylaod: err.response.data
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// GET Post
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get('/api/posts')
+    .then(res => 
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
       })
     )
+    .catch(err => 
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+// GET Post
+export const getPost = id => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get(`/api/posts/${id}`)
+    .then(res => 
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err => 
+      dispatch({
+        type: GET_POST,
+        payload: null
+      })
+    );
+};
+
+
+
+// Delete post
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(res => 
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err => 
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Add Like
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err => 
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Remove Like
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err => 
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// ADD POST
+export const addComment = (postId, commentData) => dispatch => {
+  dispatch(clearErrors());
+  axios
+    .post(`/api/posts/comment/${postId}`, commentData)
+    .then(res => 
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err => 
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// DELETE commnet
+export const deleteComment = (postId, commentId) => dispatch => {
+  axios
+    .delete(`/api/posts/comment/${postId}/${commentId}`)
+    .then(res => 
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err => 
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Set loading state
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  }
 }
+
+//CLEAR errors
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS
+  }
+}
+
